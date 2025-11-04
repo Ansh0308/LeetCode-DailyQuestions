1b639b2f80fbb9932b79e9eff9276a36558e35fd
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2025-11-04 15:57:27 +0530

    0046-permutations 0 ms (100%) 10.6 MB (69.54%) 11-04-2025 Medium .cpp

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void helper(vector<int>& nums,vector<vector<int>> &ans,int index=0){
+        if(index==nums.size()){
+            ans.push_back({nums});
+            return;
+        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[i],nums[index]);
+            helper(nums,ans,index+1);
+            swap(nums[i],nums[index]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        helper(nums,ans);
+        return ans;
+        
+    }
+};
