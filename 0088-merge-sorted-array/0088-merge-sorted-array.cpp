c9c57a8932fcffb2a45a55775cef84d4e7e6318b
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2025-12-23 12:27:13 +0530

    0088-merge-sorted-array 0 ms (100%) 12.2 MB (70.15%) 12-23-2025 .cpp Easy

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-       int i=m-1,j=n-1,k=m+n-1;
-       while(j>=0 && i>=0 && k>=0){
-        if(nums1[i]>nums2[j]){
-            nums1[k--]=nums1[i--];
-        }
-        else if(nums2[j]>nums1[i]){
-            nums1[k--]=nums2[j--];
-        }
-        else{
-            nums1[k--]=nums1[i--];
-        }
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0 && k>=0){
+            if(i>=0 && j>=0 && nums1[i]>=nums2[j]){
+                swap(nums1[k--],nums1[i--]);
+            }
+            else if(j>=0){
+                swap(nums1[k--],nums2[j--]);
+            }
 
-       }
-       while(j>=0 && k>=0){
-        nums1[k--]=nums2[j--];
-       }
-       while(i>=0 && k>=0){
-        nums1[k--]=nums1[i--];
-       }
-        
+        }
+        while(i>=0 && k>=0){
+            swap(nums1[k--],nums1[i--]);
+        }
+         while(j>=0 && k>=0){
+            swap(nums1[k--],nums2[j--]);
+        }
     }
 };
