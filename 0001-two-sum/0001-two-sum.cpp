d3bbdbdc653ab942131700552d86fd399f3b066b
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2025-12-23 10:02:16 +0530

    0001-two-sum 0 ms (100%) 14.8 MB (53.26%) 12-23-2025 .cpp Easy

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> ma;
-        for(int i=0;i<nums.size();i++){
-            int first_ele=nums[i];
-            int second_ele=target-first_ele;
-            if(ma.find(second_ele)!=ma.end()){
-                return {i,ma[second_ele]};
-            }
-            ma[first_ele]=i;
-
+       unordered_map<int,int> freq;
+       for(int i=0;i<nums.size();i++){
+        if(freq.find(target-nums[i])!=freq.end()){
+            return {freq[target-nums[i]],i};
         }
-        return {-1,-1};
-    //     vector<pair<int,int>> s;
-    //     for(int i=0;i<nums.size();i++){
-    //         s.push_back({nums[i],i});
-    //     }
-    //     sort(s.begin(),s.end());
-    //     int start=0,end=nums.size()-1;
-    //     while(start<=end){
-    //         if(s[start].first+s[end].first==target)return{s[start].second,s[end].second};
-    //         if(s[start].first+s[end].first>=target){
-    //             end--;
-    //         }
-    //         else{
-    //             start++;
-    //         }
-            
-    //     }
-
+        else{
+            freq[nums[i]]=i;
+        }
+       }
+       return {-1,-1};
         
-    //    return {-1,-1}; 
+        }
     
-    }
+
 };
