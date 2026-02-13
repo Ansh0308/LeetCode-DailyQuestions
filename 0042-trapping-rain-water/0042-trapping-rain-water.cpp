5356c4657465b891b0c8e1a4b83a59677d86a611
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-02-13 11:09:02 +0530

    0042-trapping-rain-water 0 ms (100%) 25.9 MB (92.94%) 02-13-2026 Hard .cpp

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,20 +3,17 @@ public:
     int trap(vector<int>& height) {
         int n=height.size();
         int ans=0;
-        vector<int> l_max(n,height[0]);
-        vector<int> r_max(n,height[n-1]);
-        int lmax=height[0],rmax=height[n-1];
-        for(int i=1;i<n;i++){
-            l_max[i]=lmax;
-            lmax=max(lmax,height[i]);
-            r_max[n-i-1]=rmax;
-            rmax=max(rmax,height[n-i-1]);
-
-        }
-        for(int i=0;i<n;i++){
-            if((min(l_max[i],r_max[i])-height[i])>0){
-                ans+=(min(l_max[i],r_max[i])-height[i]);
+        int L=0,R=n-1,lmax=0,rmax=0;
+        while(L<R && L<n && R>=0){
+            lmax=max(lmax,height[L]);
+            rmax=max(rmax,height[R]);
+            if(lmax<rmax){
+                ans+=lmax-height[L++];
             }
+            else{
+                ans+=rmax-height[R--];
+            }
+
         }
         return ans;
         
