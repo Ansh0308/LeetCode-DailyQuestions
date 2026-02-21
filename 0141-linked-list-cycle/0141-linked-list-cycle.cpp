6b571afd96c64b10bd02d535212b14b427192df2
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-02-21 21:34:37 +0530

    0141-linked-list-cycle 8 ms (82.72%) 11.8 MB (53.15%) 02-21-2026 Easy .cpp

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,16 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==nullptr || head->next==nullptr)return false;
+        if(!head || !(head->next))return false;
         ListNode* slow=head;
         ListNode* fast=head;
         while(fast!=nullptr && fast->next!=nullptr){
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast)return true;
+            if(slow==fast)break;
         }
-        return false;
-
+        return slow==fast;
         
     }
 };
