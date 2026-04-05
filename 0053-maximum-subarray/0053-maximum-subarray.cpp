0baacfa7dc959ee65359949f3be08f027bf47844
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-04-05 20:56:39 +0530

    0053-maximum-subarray 0 ms (100%) 71.6 MB (98.03%) 04-05-2026 Medium .cpp

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if(nums.size()==1)return nums[0];
-        int lsum=nums[0],gsum=INT_MIN;
-        for(int i=1;i<nums.size();i++){
+        int gsum=INT_MIN;
+        int lsum=0;
+        for(int ele:nums){
+            lsum=max(ele,lsum+ele);
             gsum=max(gsum,lsum);
-            lsum=max(nums[i],lsum+nums[i]);
-            
         }
-        gsum=max(gsum,lsum);
         return gsum;
         
     }
