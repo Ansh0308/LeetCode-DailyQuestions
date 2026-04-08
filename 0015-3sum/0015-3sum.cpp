1fb41176b21a3ad792eb7f16edc0ebab6118a5e6
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-04-08 12:04:14 +0530

    0015-3sum N/A (0%) N/A (0%) 04-08-2026 Medium .cpp

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            // Skip duplicates for i
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int j = i + 1, k = n - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum < 0) j++;
-                else if (sum > 0) k--;
-                else {
-                    ans.push_back({nums[i], nums[j], nums[k]});
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        set<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>0)k--;
+                else if(sum<0)j++;
+                else{
+                    ans.insert({nums[i],nums[j],nums[k]});
                     j++;
-                    k--;
-
-                    // Skip duplicates for j
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    // Skip duplicates for k
-                    while (j < k && nums[k] == nums[k + 1]) k--;
+                k--;
+                while(j<n-1 && nums[j]==nums[j-1])j++;
+                while(k>0 && k<n-1 && nums[k]==nums[k-1])k--;
                 }
+                
+                
             }
         }
+        return {ans.begin(),ans.end()};
 
-        return ans;
+        
+        
     }
 };
