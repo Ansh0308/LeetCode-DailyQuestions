b07d35b08c0e9a5f66b3972aca95decb33fcb1ab
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-04-23 19:25:53 +0530

    0704-binary-search 0 ms (100%) 31.4 MB (7.57%) 04-23-2026 .cpp Easy

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    
     int search(vector<int>& nums, int target) {
         int n=nums.size();
-        if(n==1 && nums[0]==target)return 0;
-        int low=0,high=n-1;
-        while(low<=high){
-            int mid =low+(high-low)/2;
+        int left=0,right=n-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
             if(nums[mid]==target)return mid;
-            if(nums[mid]<target){
-                low=mid+1;
-            }
+            if(nums[mid]<target)left=mid+1;
             else{
-                high=mid-1;
+                right=mid-1;
             }
         }
         return -1;
+        
     }
 };
