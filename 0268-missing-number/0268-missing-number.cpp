056b3525917763ee63464693ee05348aaabe7e7a
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-04-24 18:03:18 +0530

    0268-missing-number 0 ms (100%) 21.8 MB (69.65%) 04-24-2026 .cpp Easy

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,14 +3,16 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int sum_1=0;
-        for(int i=0;i<=nums.size();i++){
-            sum_1^=i;
-        }
+        // for(int i=0;i<=nums.size();i++){
+        //     sum_1^=i;
+        // }
         for(int i=0;i<nums.size();i++){
             sum_1^=nums[i];
+            sum_1^=i;
             
         }
+        return sum_1^nums.size();
        
-        return sum_1;
+        
     }
 };
