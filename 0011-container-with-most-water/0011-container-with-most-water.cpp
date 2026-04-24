1c45c6aa690e017b413f149480ffb33aac5c850c
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-04-24 17:05:48 +0530

    0011-container-with-most-water 0 ms (100%) 62.8 MB (97.66%) 04-24-2026 .cpp Medium

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int maxArea(vector<int>& nums) {
-        int left=0,right=nums.size()-1;
-        int larea=0,garea=INT_MIN;
+        int n=nums.size();
+        int left=0,right=n-1;
+        int gArea=INT_MIN;
         while(left<right){
-            int width=right-left;
-            int height=min(nums[left],nums[right]);
-            larea=width*height;
-            garea=max(garea,larea);
+            int cArea=(right-left)*min(nums[left],nums[right]);
+            gArea=max(gArea,cArea);
             if(nums[left]>nums[right])right--;
             else{
                 left++;
             }
+
         }
-        return garea;
+        return gArea;
         
     }
 };
