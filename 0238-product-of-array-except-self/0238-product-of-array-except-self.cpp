808729314f879d2d9d44d34cc2b5200e4568d1ad
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-04-24 17:52:18 +0530

    0238-product-of-array-except-self 7 ms (6.54%) 40 MB (99.82%) 04-24-2026 .cpp Medium

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int> ans(n,1);
-        int pre=nums[0];
+        vector<int> mul(n,1);
+        //pre
         for(int i=1;i<n;i++){
-            ans[i]*=pre;
-            pre*=nums[i];
-        }
-        int suf=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            ans[i]*=suf;
-            suf*=nums[i];
-        }
-        return ans;
-        
+            mul[i]=mul[i-1]*nums[i-1];
+         }
+        int pro=1;
+        int nu=nums[n-1];
+         for(int i=n-2;i>=0;i--){
+            mul[i]*=pro*nu;
+            pro*=nums[i+1];
+            nu=nums[i];
+         }
+        return mul;
         
     }
 };
