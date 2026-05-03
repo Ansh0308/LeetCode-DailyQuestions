81ce53ba129e61a8e65fd16771e77d25890ff85e
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-05-03 16:06:47 +0530

    0004-median-of-two-sorted-arrays 3 ms (28.47%) 95.4 MB (38.12%) 05-03-2026 .cpp Hard

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size();
-        int n=nums2.size();
-        int t_length=m+n;
         nums1.insert(nums1.end(),nums2.begin(),nums2.end());
         sort(nums1.begin(),nums1.end());
-        if(t_length%2!=0)return nums1[t_length/2];
-        return double(nums1[t_length/2]+nums1[(t_length/2)-1])/2;
-        
+        int len=nums1.size();
+        return (len%2==0)?double((nums1[len/2]+nums1[(len)/2.0-1])/2.0):nums1[len/2];
     }
 };
