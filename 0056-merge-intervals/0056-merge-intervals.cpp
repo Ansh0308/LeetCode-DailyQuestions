45556484ff2f67d3a83f52421db9f447595c87b3
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-05-03 14:43:08 +0530

    0056-merge-intervals 3 ms (89.27%) 24.8 MB (12.88%) 05-03-2026 .cpp Medium

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        sort(intervals.begin(),intervals.end());
-        for(vector<int> range:intervals){
+        for(vector<int> ele:nums){
             if(ans.size()==0){
-                ans.push_back(range);
+                ans.push_back(ele);
                 continue;
             }
-            if(ans[ans.size()-1][1]>=range[0] && ans[ans.size()-1][1]>=range[1])continue;
-            else if(ans[ans.size()-1][1]>=range[0]){
-                ans[ans.size()-1][1]=range[1];
+            if(ans[ans.size()-1][1]>=ele[0] && ans[ans.size()-1][1]>=ele[1])continue;
+            if(ans[ans.size()-1][1]>=ele[0]){
+                ans[ans.size()-1][1]=ele[1];
             }
             else{
-                ans.push_back(range);
+                ans.push_back(ele);
             }
         }
         return ans;
+        
     }
 };
