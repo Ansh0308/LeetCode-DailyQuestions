71f7de29035945df7d76dc1e3ef78c86c71ef207
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-05-09 11:45:18 +0530

    0724-find-pivot-index 3 ms (20.53%) 37 MB (13.78%) 05-09-2026 .cpp Easy

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> preSum(n,0);
+        vector<int> sufSum(n,0);
+        for(int i=1;i<n;i++){
+            preSum[i]=preSum[i-1]+nums[i-1];
+           
+        }
+        for(int i=n-2;i>=0;i--){
+            sufSum[i]=sufSum[i+1]+nums[i+1];
+        }
+        for(int i=0;i<n;i++){
+            if(preSum[i]==sufSum[i])return i;
+        }
+        return -1;
+
+        
+    }
+};
