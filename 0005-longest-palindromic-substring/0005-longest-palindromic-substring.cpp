f3d3e071ed5520ca0474b08bc9e8400285958521
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-05-20 13:14:29 +0530

    0005-longest-palindromic-substring 3 ms (98.05%) 9.3 MB (92.92%) 05-20-2026 .cpp Medium

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,39 +2,35 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n=s.size();
-        if(n<=1)return s;
-
-        int len=0;
-        int st=0,end=0;
-        //odd len ans
-        for(int i=0;i<s.size();i++){
+        if(n==1)return s;
+        int maxL=0,st=0,end=0;
+        
+        for(int i=0;i<n;i++){
             int l=i,r=i;
-            
             while(l>=0 && r<n && s[l]==s[r]){
+                if(maxL<r-l+1){
+                    maxL=r-l+1;
+                    st=l,end=r;
+                }
                 l--;
                 r++;
             }
-            if(r-l-1>len){
-                len=r-l-1;
-                st=l+1;
-                end=r-1;
-            }
-            
-
         }
-        for(int i=0;i<n;i++){
-            int prev=i,next=i+1;
-            while(prev>=0 && next<n && s[prev]==s[next]){
-                prev--;
-                next++;
-            }
-            if(next-prev-1>len){
-                len=next-prev-1;
-                st=prev+1;
-                end=next-1;
+        
+            for(int i=0;i<n;i++){
+            int l=i,r=i+1;
+            while(l>=0 && r<n && s[l]==s[r]){
+                if(maxL<r-l+1){
+                    maxL=r-l+1;
+                    st=l,end=r;
+                }
+                l--;
+                r++;
             }
         }
-        return s.substr(st,len);
+        
+        if(maxL==0)maxL=1;
+    return s.substr(st,maxL);
         
     }
 };
