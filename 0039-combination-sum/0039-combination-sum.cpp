fec4be60667ac33a65d02f8670f94379abce368f
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-05-24 10:42:09 +0530

    0039-combination-sum 83 ms (7.69%) 71.1 MB (6.59%) 05-24-2026 .cpp Medium

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-void helper(vector<int>& nums,int target,vector<vector<int>>& ans,int index=0,int sum=0,vector<int> temp={}){
-    if(sum==target){
-    ans.push_back(temp);
-    return;
-}
-if(index==nums.size() || sum>target){
-    
-    return;
-}
+    void helper(vector<int>& nums, int target,vector<vector<int>> & ans,vector<int> curr,int sum,int idx){
+        if(sum==target){
+            ans.push_back(curr);
+            return;
+        }
+        if(idx>=nums.size() || sum>target)return;
+        curr.push_back(nums[idx]);
+        helper(nums,target,ans,curr,sum+nums[idx],idx);
+        curr.pop_back();
+        helper(nums,target,ans,curr,sum,idx+1);
 
-//recursion
-temp.push_back(nums[index]);
-helper(nums,target,ans,index,sum+nums[index],temp);
-temp.pop_back();
-helper(nums,target,ans,index+1,sum,temp);
-
-
-
-}
+    }
     vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         vector<vector<int>> ans;
-        helper(nums,target,ans);
+        helper(nums,target,ans,{},0,0);
         return ans;
         
     }
