ae3df5c538e3c7ba8f05a8327512dea5d2a2bb8c
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-06-04 19:10:52 +0530

    0349-intersection-of-two-arrays 4 ms (20.96%) 14.3 MB (60.82%) 06-04-2026 .cpp Easy

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> st;
-        unordered_set<int> ans;
-        for(int ele:nums1){
-            st.insert(ele);
-        }
-        for(int ele :nums2){
-            if(st.find(ele)!=st.end()){
-                ans.insert(ele);
+        unordered_set<int> st(nums1.begin(), nums1.end());
+        vector<int> ans;
+
+        for (int x : nums2) {
+            if (st.count(x)) {
+                ans.push_back(x);
+                st.erase(x);   // prevents duplicates
             }
         }
-        return {ans.begin(),ans.end()};
 
-        
+        return ans;
     }
 };
