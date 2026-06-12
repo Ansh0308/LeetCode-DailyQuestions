4de9e048bf3842d93faf42d3089456181507f19d
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-06-12 20:04:26 +0530

    0006-zigzag-conversion 2 ms (81.76%) 11 MB (86.69%) 06-12-2026 .cpp Medium

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if(numRows<=1)return s;
-        string ans="";
-        for(int i=0;i<numRows;i++){
-            for(int j=i;j<s.size();j+=(numRows-1)*2){
-                ans+=s[j];
-                if(i>0 && i<numRows-1 && j+((numRows-1)*2)-2*i<s.size()){
-                    ans+=s[j+((numRows-1)*2)-2*i];
+        if(numRows == 1) return s;  // avoid infinite loop
 
+        int n = s.size();
+        string ans = "";
+
+        for(int i = 0; i < numRows; i++) {
+            int j = i;
+
+            while(j < n) {
+                ans += s[j];
+
+                // middle rows have an extra diagonal character
+                if(i != 0 && i != numRows - 1) {
+                    int diag = j + (numRows - 1) * 2 - 2 * i;
+
+                    if(diag < n) {
+                        ans += s[diag];
+                    }
                 }
-                
 
+                j += (numRows - 1) * 2;
             }
         }
+
         return ans;
-        
     }
 };
