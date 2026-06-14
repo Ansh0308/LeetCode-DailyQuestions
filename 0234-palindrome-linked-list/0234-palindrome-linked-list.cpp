981b6bf6194fd1159184e9bf4fee94674ddab9dd
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-06-14 10:44:17 +0530

    0234-palindrome-linked-list 62 ms (5.19%) 125.6 MB (30.29%) 06-14-2026 .cpp Easy

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* temp=head;
+        string ans="";
+        while(temp!=nullptr){
+            ans+=to_string(temp->val);
+            temp=temp->next;
+        }
+        int n=ans.size();
+        for(int i=0;i<n/2;i++){
+            if(ans[i]!=ans[n-1-i])return false;
+        }
+        return true;
+        
+    }
+};
