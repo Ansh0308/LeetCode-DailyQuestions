efc1e89ba440ac3561efcdfd2a1852744a28e770
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-06-21 22:02:53 +0530

    0152-maximum-product-subarray 0 ms (100%) 17.8 MB (22.77%) 06-21-2026 .cpp Medium

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-
     int maxProduct(vector<int>& nums) {
-       int maxPro=nums[0],minPro=nums[0];
-       int ans=maxPro;
-       for(int i=1;i<nums.size();i++){
+        if(nums.size()==1)return nums[0];
+      int minPro=nums[0],maxPro=nums[0];
+      int ans=INT_MIN;
+      for(int i=1;i<nums.size();i++){
+        ans=max(maxPro,ans); 
         if(nums[i]<0){
             swap(maxPro,minPro);
         }
-        maxPro=max(maxPro*nums[i],nums[i]);
-        minPro=min(minPro*nums[i],nums[i]);
-        ans=max(ans,maxPro);
-       }
-       return ans;
+        
+        maxPro=max(nums[i],nums[i]*maxPro);
+        minPro=min(nums[i],nums[i]*minPro);
+        
+        
+      }
+      ans=max(maxPro,ans); 
+      return ans;
     }
 };
