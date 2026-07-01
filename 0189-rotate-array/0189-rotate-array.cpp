3220caf06b20f2de478765c3f217236a0aecd987
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-07-01 12:04:49 +0530

    0189-rotate-array 4 ms (4.25%) 29.5 MB (93.35%) 07-01-2026 .cpp Medium

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    void reverse(vector<int>& nums,int st,int end){
-        while(st<=end){
+    void reverse(vector<int>& nums, int st,int end){
+        while(st<end){
             swap(nums[st++],nums[end--]);
-
         }
     }
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        if(n<=1)return;
         k=k%n;
         reverse(nums,0,n-1);
         reverse(nums,0,k-1);
         reverse(nums,k,n-1);
+        return;
         
     }
 };
