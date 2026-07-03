eef933d4d878afb0234e8058778fb0e9f166fd09
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-07-03 11:28:54 +0530

    0118-pascals-triangle 0 ms (100%) 9.6 MB (89.99%) 07-03-2026 .cpp Easy

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int n) {
+    vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        
-        for(int i=0;i<n;i++){
+        for(int i=0;i<numRows;i++){
             vector<int> row(i+1,1);
-            for(int j=1;j<i;j++){
-                row[j]=ans[i-1][j-1]+ans[i-1][j];
-                
-            }
             ans.push_back(row);
+            for(int j=1;j<i;j++){
+                ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
+            }
         }
         return ans;
         
