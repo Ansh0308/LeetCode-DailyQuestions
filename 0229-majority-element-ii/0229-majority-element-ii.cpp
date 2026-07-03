c0c462c5d91fabe1257088917a62f2d8d9a2bdcc
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-07-03 11:45:06 +0530

    0229-majority-element-ii 7 ms (40.34%) 23.9 MB (40.79%) 07-03-2026 .cpp Medium

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,16 +2,35 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int> mp;
-        unordered_set<int> ans;
-        for(int ele:nums){
-            mp[ele]++;
-            if(mp[ele]>n/3){
+        if(n/3==0){
+            unordered_set<int> ans;
+            for(int ele:nums){
                 ans.insert(ele);
+            }
+            return {ans.begin(),ans.end()};
+        }
+        // unordered_map<int,int> mp;
+        // unordered_set<int> ans;
+        // for(int ele:nums){
+        //     mp[ele]++;
+        //     if(mp[ele]>n/3){
+        //         ans.insert(ele);
+        //     }
+        // }
+        // return {ans.begin(),ans.end()};
+        sort(nums.begin(),nums.end());
+        int freq=1;
+        unordered_set<int> ans;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1])freq++;
+            else{
+                freq=1;
+            }
+            if(freq>n/3){
+                ans.insert(nums[i]);
             }
         }
         return {ans.begin(),ans.end()};
-
         
     }
 };
