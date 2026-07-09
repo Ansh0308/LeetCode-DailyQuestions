bd3bef7a89c9dca482cb39c50ef4668f61691857
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-07-09 14:47:17 +0530

    0283-move-zeroes 0 ms (100%) 23.8 MB (82.87%) 07-09-2026 Easy .cpp

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
-        int n = nums.size();
-       
-    
-
-        for (int i = 0; i < n; i++) {
-            if(nums[i]==0){
-               for(int j=i+1;j<n;j++){
-                if(nums[j]!=0){
-                    swap(nums[i],nums[j]);
-                    break;
-                }
-               }
+        int idx=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[idx++],nums[i]);
             }
         }
-
-            
+        
     }
 };
