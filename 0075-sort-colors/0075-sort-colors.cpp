c79d740a95aecd5f7ebd084878a9b52a197d7dbc
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-07-10 09:20:13 +0530

    0075-sort-colors 0 ms (100%) 11.7 MB (49.09%) 07-10-2026 Medium .cpp

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        
-    int n=nums.size();
-    int low =0,mid=0,high=n-1;
-    while(mid<=high){
-        if(nums[mid]==0){
-            swap(nums[mid],nums[low]);
-            low++;
-            mid++;
+        int n=nums.size();
+        int st=0,mid=0,end=n-1;
+        while(mid<=end && st<=end){
+            if(nums[mid]==2){
+                swap(nums[end--],nums[mid]);
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid++],nums[st++]);
+            }
         }
-        else if(nums[mid]==2){
-            swap(nums[mid],nums[high]);
-            high--;
-            
-
-        }
-        else{
-            mid++;
-        }
-    }
-    
-    
+        return ;
         
     }
 };
