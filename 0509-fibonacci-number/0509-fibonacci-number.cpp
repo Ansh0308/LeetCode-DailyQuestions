3327f3741a40ee12e986d32bfe12ca12aa1f3d91
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-07-17 20:53:07 +0530

    0509-fibonacci-number 13 ms (32.27%) 7.9 MB (38.57%) 07-17-2026 Easy .cpp

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
-    int helper(int n,int a=0,int b=1){
-        if(n==0)return a;
-        else if(n==1)return b;
-        
-        int c=a+b;
-        return helper(n-1,b,c);
-    }
     int fib(int n) {
-        return helper(n);
+        return (n<=1)?n:fib(n-1)+fib(n-2);
+        
     }
-    
 };
