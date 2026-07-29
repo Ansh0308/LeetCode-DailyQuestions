f2c4a51c307419aa72208f9ad2908357fdeb048b
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-07-29 10:40:51 +0530

    0088-merge-sorted-array 0 ms (100%) 12.4 MB (22.13%) 07-29-2026 Easy .cpp

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    void merge(vector<int>& num1, int m, vector<int>& num2, int n) {
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       int i=m-1,j=n-1,k=m+n-1;
+       while(j>=0 && i>=0 && k>=0){
+        if(nums1[i]>nums2[j]){
+            nums1[k--]=nums1[i--];
+        }
+        else if(nums2[j]>nums1[i]){
+            nums1[k--]=nums2[j--];
+        }
+        else{
+            nums1[k--]=nums1[i--];
+        }
+
+       }
+       while(j>=0 && k>=0){
+        nums1[k--]=nums2[j--];
+       }
+       while(i>=0 && k>=0){
+        nums1[k--]=nums1[i--];
+       }
         
-        int p1=m-1,p2=n-1,p=m+n-1;
-        while(p1>=0 && p2>=0 && p>=0){
-            if(num1[p1]>num2[p2])num1[p--]=num1[p1--];
-            else{
-                num1[p--]=num2[p2--];
-            }
-        }
-        while(p1>=0 && p>=0){
-            num1[p--]=num1[p1--];
-        }
-        while(p2>=0 && p>=0){
-            num1[p--]=num2[p2--];
-        }
     }
 };
