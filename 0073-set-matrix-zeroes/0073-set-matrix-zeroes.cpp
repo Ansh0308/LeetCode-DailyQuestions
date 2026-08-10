efae5d49bfaf0cb470e7e6c1422f366dd73307ee
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-08-10 09:17:17 +0530

    0073-set-matrix-zeroes 0 ms (100%) 21.1 MB (17.47%) 08-10-2026 Medium .cpp

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& arr) {
-        int r=arr.size(),c=arr[0].size();
-        unordered_set<int> rows;
-        unordered_set<int> cols;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(arr[i][j]==0){
-                    rows.insert(i);
-                    cols.insert(j);
-                }
+    void setZeroes(vector<vector<int>>& nums) {
+        int m=nums.size(),n=nums[0].size();
+        unordered_set<int> rows,cols;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(nums[i][j]==0){
+                rows.insert(i);
+                cols.insert(j);}
             }
         }
-        for(int row:rows){
-            for(int j=0;j<c;j++){
-                arr[row][j]=0;
+        for(int ele : rows){
+            for(int j=0;j<n;j++){
+                nums[ele][j]=0;
             }
         }
-        for(int col:cols){
-            for(int i=0;i<r;i++){
-                arr[i][col]=0;
+        for(int ele:cols){
+            for(int i=0;i<m;i++){
+                nums[i][ele]=0;
             }
         }
-        
     }
 };
