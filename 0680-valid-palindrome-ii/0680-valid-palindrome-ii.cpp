4e547ed7913369b5ec9cbd750e9c1491c0a8419a
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-08-13 10:03:42 +0530

    0680-valid-palindrome-ii 8 ms (20.73%) 26.7 MB (13.72%) 08-13-2026 Easy .cpp

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    bool isPal(string s,int i,int j){
-       
-        while(i<=j){
-            if(s[i]!=s[j])return false;
-            i++;
-            j--;
+    bool isPal(string s,int st,int end){
+        while(st<end){
+            if(s[st]!=s[end])return false;
+            st++;
+            end--;
         }
         return true;
     }
     bool validPalindrome(string s) {
-        
-        int n=s.size();
-        int st=0,end=n-1;
-        while(st<=end){
-            if(s[st]==s[end]){
-                st++;
-                end--;
+        int i=0,j=s.size()-1;
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            
             }
             else{
-                return isPal(s,st+1,end) || isPal(s,st,end-1);
-
+                return isPal(s,i+1,j) || isPal(s,i,j-1);
             }
         }
         return true;
-        
     }
 };
