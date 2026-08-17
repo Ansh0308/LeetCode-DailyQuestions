4ac19e7c4cf5230ef0b984de1db01792e9316e76
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-08-17 08:40:29 +0530

    0796-rotate-string 3 ms (5.14%) 8.1 MB (28.23%) 08-17-2026 Easy .cpp

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,44 +1,16 @@
 class Solution {
 public:
+   
     bool rotateString(string s, string goal) {
-        int n = s.size();
-        int m = goal.size();
-
-        if (n != m) return false;
-
-        int i = 0;
-
-        while (i < n) {
-            if (s[i] == goal[0]) {
-
-                int j = 0;
-
-                // Match suffix of s with prefix of goal
-                while (i + j < n && s[i + j] == goal[j]) {
-                    j++;
-                }
-
-                // Entire string matched
-                if (j == n)
-                    return true;
-
-                bool temp = true;
-
-                // Match remaining prefix of s with remaining suffix of goal
-                for (int a = 0; a < n - j; a++, j++) {
-                    if (s[a] != goal[j]) {
-                        temp = false;
-                        break;
-                    }
-                }
-
-                if (temp)
-                    return true;
+        int n=s.size();
+        int m=goal.size();
+        if(m!=n)return false;
+        string merge=s+s;
+        for(int i=0;i<merge.size();i++){
+            if(merge[i]==goal[0]){
+                if(merge.substr(i,m)==goal)return true;
             }
-
-            i++;
         }
-
         return false;
     }
 };
