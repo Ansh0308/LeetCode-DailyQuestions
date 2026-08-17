dba2943a82809eec24a7d6082d66d9b27c3ef062
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-08-17 09:15:29 +0530

    0205-isomorphic-strings 2 ms (48.74%) 9.5 MB (30.84%) 08-17-2026 Easy .cpp

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.length()!=t.length())return false;
-        unordered_map<char,char> mp;
-        unordered_set<char> keys;
-        for(int i=0;i<s.length();i++){
+        int n=s.size(),m=t.size();
+        if(n!=m)return false;
+        unordered_map<int,int> mp;
+        unordered_set<int> keys;
+        for(int i=0;i<n;i++){
             if(mp.find(s[i])!=mp.end() && mp[s[i]]!=t[i])return false;
-            if(mp.find(s[i])==mp.end() && keys.find(t[i])!=keys.end())return false;
+            if(keys.find(t[i])!=keys.end() && mp.find(s[i])==mp.end())return false;
             mp[s[i]]=t[i];
             keys.insert(t[i]);
+
         }
         return true;
-
-        
     }
 };
