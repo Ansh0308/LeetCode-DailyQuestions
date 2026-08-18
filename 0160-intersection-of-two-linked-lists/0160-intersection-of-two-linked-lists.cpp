8e7a628c1fc2cb049273656562e423435ada86b3
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-08-18 10:41:41 +0530

    0160-intersection-of-two-linked-lists 57 ms (54.47%) 24 MB (25.7%) 08-18-2026 Easy .cpp

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,18 +9,57 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> sets;
-        ListNode* temp=headA;
-        while(temp!=nullptr){
-            sets.insert(temp);
-            temp=temp->next;
+        // unordered_set<ListNode*> sets;
+        // ListNode* temp=headA;
+        // while(temp!=nullptr){
+        //     sets.insert(temp);
+        //     temp=temp->next;
+        // }
+        // temp=headB;
+        // while(temp!=nullptr){
+        //     if(sets.find(temp)!=sets.end())return temp;
+        //     temp=temp->next;
+        // }
+
+        //Optimised
+        ListNode* temA=headA;
+        ListNode* temB=headB;
+        int m=1,n=1;
+        while(temA->next!=nullptr){
+            temA=temA->next;
+            m++;
         }
-        temp=headB;
-        while(temp!=nullptr){
-            if(sets.find(temp)!=sets.end())return temp;
-            temp=temp->next;
+        while(temB->next!=nullptr){
+            temB=temB->next;
+            n++;
         }
-        return nullptr;
+        temA=headA;
+        temB=headB;
+        if(m>n){
+            int diff=m-n;
+            
+            while(diff!=0){
+                temA=temA->next;
+                diff--;
+
+            }
+            
+        }
+        else{
+            int diff=n-m;
+            while(diff!=0){
+                temB=temB->next;
+                diff--;
+                
+            }
+
+        }
+        while(temA!=temB){
+            temA=temA->next;
+            temB=temB->next;
+        }
+
+        return temA;
         
     }
 };
