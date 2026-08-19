3d4d9481b7d39925f426a55fc6136fc3ab693840
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-08-19 09:57:06 +0530

    0083-remove-duplicates-from-sorted-list 0 ms (100%) 16.5 MB (10.51%) 08-19-2026 Easy .cpp

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -13,14 +13,15 @@ public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head==nullptr || head->next==nullptr)return head;
         ListNode* temp=head;
-        while(temp!=nullptr && temp->next!=nullptr){
-            while(temp!=nullptr && temp->next!=nullptr && temp->val==temp->next->val){
+        while(temp!=nullptr){
+            while(temp->next!=nullptr && temp->val==temp->next->val){
                 ListNode* del=temp->next;
-                temp->next=del->next;
-                delete(del);
+                temp->next=temp->next->next;
+                delete del;
             }
             temp=temp->next;
         }
         return head;
+        
     }
 };
