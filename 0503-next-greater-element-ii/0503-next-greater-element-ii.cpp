256c983b857a299f2a82ebb5cda201999a2d25ac
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-09-15 12:36:26 +0530

    0503-next-greater-element-ii 7 ms (36%) 28.7 MB (19.34%) 09-15-2026 .cpp Medium

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,16 +2,34 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            nums.push_back(nums[i]);
-        }
+        // for(int i=0;i<n-1;i++){
+        //     nums.push_back(nums[i]);
+        // }
+        // vector<int> ans(n,-1);
+        // stack<int> st;
+        // for(int i=0;i<nums.size();i++){
+        //     while(!st.empty() && nums[st.top()]<nums[i]){
+        //         int idx=st.top();
+        //         st.pop();
+        //         ans[idx%n]=nums[i];
+        //     }
+        //     st.push(i);
+        // }
         vector<int> ans(n,-1);
         stack<int> st;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             while(!st.empty() && nums[st.top()]<nums[i]){
                 int idx=st.top();
                 st.pop();
-                ans[idx%n]=nums[i];
+                ans[idx]=nums[i];
+            }
+            st.push(i);
+        }
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<nums[i]){
+                int idx=st.top();
+                st.pop();
+                ans[idx]=nums[i];
             }
             st.push(i);
         }
