fd3a439e5908b8a981024cdd25b948c11c8c5402
Ansh Raythatha <141222185+Ansh0308@users.noreply.github.com>
2026-09-15 12:14:46 +0530

    0503-next-greater-element-ii 0 ms (100%) 29.5 MB (9.53%) 09-15-2026 .cpp Medium

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,32 +2,19 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
-        int maxele=0;
-        for(int i=0;i<n;i++){
-            if(nums[maxele]<nums[i]){
-                maxele=i;
-            }
+        for(int i=0;i<n-1;i++){
+            nums.push_back(nums[i]);
         }
         vector<int> ans(n,-1);
         stack<int> st;
-        st.push(maxele);
-        for(int i=maxele-1;i!=maxele;i--){
-            if(i<0)i=n-1;
-            if(i==maxele)break;
-            while(!st.empty()){
-                if(nums[st.top()]>nums[i]){
-                    ans[i]=nums[st.top()];
-                    break;
-                }
+        for(int i=0;i<nums.size();i++){
+            while(!st.empty() && nums[st.top()]<nums[i]){
+                int idx=st.top();
                 st.pop();
+                ans[idx%n]=nums[i];
             }
-            if(st.empty())ans[i]=-1;
             st.push(i);
-
         }
         return ans;
-         
-
-        
     }
 };
